676174fb3cce74f2251a2a2d29b5eafec8b80932
mae <contact@mae.global>
2016-11-18 16:41:12 +0000

    added debug test

--- rma/rma_test.go
+++ rma/rma_test.go
@@ -1,15 +1,31 @@
 package rma
 
 import (
+	"os"	
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 
 	"fmt"
 )
 
+var localtest bool
+
+func init() {
+	debug := os.Getenv("RIGO2_DEBUG")
+	if debug == "testing" {
+		localtest = false
+	} else {
+		localtest = true
+	}
+}
+
 func Test_RMA(t *testing.T) {
 
 	Convey("RMA test",t,func() {
+
+		if !localtest {
+			t.Skip()
+		}
 
 		asset,err := Load("/Materials/Fabric/beige_fabric.rma")
 		So(err,ShouldBeNil)
@@ -18,6 +34,10 @@ func Test_RMA(t *testing.T) {
 	})
 
 	Convey("RMA load library",t,func() {
+
+		if !localtest {
+			t.Skip()
+		}
 
 		lib,err := LoadLibrary()
 		So(err,ShouldBeNil)
